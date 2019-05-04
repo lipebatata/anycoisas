558e10f12e8c93ecf63b78c071d1903db213f749
Luiz Felipe <lfvmolnar@gmail.com>
2019-05-04 11:35:17 -0300

    começo da cena de batalha

---
+++ Assets/scripts/batalha.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class batalha : MonoBehaviour
+{
+    public class Personagem
+    {
+        int vida = 100;
+        int dano = 10;
+
+        void Atacar()
+        {
+            vida = vida - dano;
+            if (vida < 1)
+            {
+                SceneManager.LoadScene("exploracao", LoadSceneMode.Additive);
+            }
+        }
+    }
+
+}
